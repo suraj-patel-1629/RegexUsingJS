a033cbec46a40c28bf19b6c5b9ba7d01f3976882
suraj patel <sp9589890205@gmail.com>
2025-03-05 12:19:26 +0530

    [Suraj] add. handling Optional part

--- Email/email.js
+++ Email/email.js
@@ -1,12 +1,13 @@
-const emailRegex = /^abc(\.[a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-zA-Z]{2,})?$/;
+const emailRegex = /^abc([._\+\-][a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-zA-Z]{2,})+$/;
 
 function validateEmail(email) {
     return emailRegex.test(email);
 }
 
-//emailUC1
+
 console.log(validateEmail("abc@bridgelabz.co"));
+console.log(validateEmail("abc@bridgelabz.co.in"));
 console.log(validateEmail("abc.xyz@bridgelabz.co.in"));
-console.log(validateEmail("abc@bridgelabz.com"));
-console.log(validateEmail("xyz@bridgelabz.co"));
-console.log(validateEmail("abc@xyz.co"));
+console.log(validateEmail("abc-xyz@bridgelabz.co.in"));
+console.log(validateEmail("abc_xyz@bridgelabz.co.in"));
+console.log(validateEmail("abc+xyz@bridgelabz.co.in"));
