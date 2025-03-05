652ce12ee22f98cee64c600acf8caff9059b06e1
suraj patel <sp9589890205@gmail.com>
2025-03-05 11:58:00 +0530

    [Suraj] add. Check for 400088B – this              should fail

--- PinCode/PinCode.js
+++ PinCode/PinCode.js
@@ -7,4 +7,7 @@ function validatePinCode(pinCode) {
 
 console.log(validatePinCode("400088"));
 console.log(validatePinCode("000088"));
+//PincodeUC2
 console.log(validatePinCode("A400088"));
+//PincodeUC3
+console.log(validatePinCode("400088B"));
