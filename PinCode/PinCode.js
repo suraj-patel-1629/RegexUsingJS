24df39f78a59591fefa21cdc4ae7694488c8e854
suraj patel <sp9589890205@gmail.com>
2025-03-05 11:52:32 +0530

    [Suraj] add. Regex pattern to Validate pincode

---
+++ PinCode/PinCode.js
@@ -0,0 +1,9 @@
+const pinCodeRegex = /^[1-9][0-9]{5}$/;
+
+function validatePinCode(pinCode) {
+    return pinCodeRegex.test(pinCode);
+}
+
+
+console.log(validatePinCode("400088"));
+console.log(validatePinCode("000088"));
